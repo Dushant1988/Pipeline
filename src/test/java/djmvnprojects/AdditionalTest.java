bf4f8c414aa5cf2266a5a32aaafb19ea4e744b85
Dushant1988 <dushant.jawalkar@gmail.com>
2020-04-28 12:49:17 +0530

    Pipeline update

---
+++ src/test/java/djmvnprojects/AdditionalTest.java
@@ -0,0 +1,24 @@
+package djmvnprojects;
+
+import org.testng.annotations.Test;
+
+public class AdditionalTest {
+
+	@Test	
+	public void Additional1()
+	{
+		System.out.println("Additional1");
+	}
+	
+	@Test	
+	public void Additional2()
+	{
+		System.out.println("Additional2");
+	}
+	
+	@Test	
+	public void Additional3()
+	{
+		System.out.println("Additional3");
+	}
+}
